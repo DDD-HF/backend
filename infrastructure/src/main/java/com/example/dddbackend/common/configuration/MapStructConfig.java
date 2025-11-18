0f6602aa816a1f60d57b914d8015856e0bf55057
Jong-Dae Park <jongdae837@gmail.com>
2025-11-18 21:30:59 +0900

    feat: cors 설정 추가

--- infrastructure/src/main/java/com/example/dddbackend/common/configuration/MapStructConfig.java
+++ infrastructure/src/main/java/com/example/dddbackend/common/configuration/MapStructConfig.java
@@ -1,12 +1,14 @@
 package com.example.dddbackend.common.configuration;
 
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
 
 @MapperConfig(
         componentModel = "spring",
         unmappedTargetPolicy = ReportingPolicy.ERROR,
-        uses = {EnumConverter.class}
+        uses = {EnumConverter.class},
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR
 )
 public interface MapStructConfig {
 }
---
+++ infrastructure/src/main/java/com/example/dddbackend/common/configuration/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.example.dddbackend.common.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .csrf(csrf -> {
+                    csrf.disable();
+                })
+                .cors(Customizer.withDefaults())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
